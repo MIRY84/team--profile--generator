64b78e09d5d734cb04748a45f8f5f8ec11e2ec7d
Mirella <mirella.dimichino@gmail.com>
2023-02-24 22:14:16 +0000

    adding comments constructor

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,8 +1,8 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 class Manager extends Employee {
-    constructor(name, id, email, github) { // testing
-        super(name, id, email);
+    constructor(name, id, email, github) { // testing features// constructor
+        super(name, id, email);//inheritance
         this.github = github;
     }
 }
