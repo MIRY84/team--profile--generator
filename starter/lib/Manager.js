dbbcd91e6b0545d870c2d74e8a6bfeee1ca31e23
Mirella <mirella.dimichino@gmail.com>
2023-02-25 11:06:26 +0000

    adding role/officenumber functions

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,10 +1,18 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 class Manager extends Employee {
-    constructor(name, id, email, github) { // testing features// constructor
+    constructor(name, id, email, officeNumber) { // testing features// constructor
         super(name, id, email);//inheritance
-        this.github = github;
+        this.officeNumber = officeNumber;
+        this.role = " Manager";
+    }
+
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+
+    getRole(){
+        return this.role;
     }
 }
-
 module.exports = Manager;// added the module to export
