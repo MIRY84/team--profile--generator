8011d943326ecacc1e47a45d160def7f7a896092
Mirella <mirella.dimichino@gmail.com>
2023-02-25 12:32:29 +0000

    added gitignore in the main to untrack the files

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+console.log("Hello")
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
